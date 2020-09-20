23c1146964d64a0dc295a88fbb94de904f446238
Barbs <barbarap095@gmail.com>
2020-09-20 20:03:45 +1000

    Updates

---
+++ precache-manifest.aec941f241b858a4da3ccf0cb860c372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df27a79c9748a1ca2d6765bb2656cde3",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "b040db031f36b4686c17",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b9c62014f02a5925f063",
+    "url": "/employee-directory/static/css/main.b182f112.chunk.css"
+  },
+  {
+    "revision": "b040db031f36b4686c17",
+    "url": "/employee-directory/static/js/2.54f25252.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.54f25252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c62014f02a5925f063",
+    "url": "/employee-directory/static/js/main.f83228d3.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
